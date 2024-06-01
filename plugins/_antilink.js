77db0fa91638b952f6c8fff3c2feaeb86a6cad80
Simo2882 <157930547+Simo2882@users.noreply.github.com>
2024-06-01 12:51:24 +0100

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -20,7 +20,7 @@ We do not allow links from other groups
 bye bye 👋🏻 *@${m.sender.split('@')[0]}*  you will be kicked out of the group ${isBotAdmin ? '' : '\n\nIM not an admin so I canT expel you :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
-            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'delete')
         } else if (!chat.antiLink) return //m.reply('')
     }
     return !0
